dc636fe02965ae356cd42f9da9199c4771096f53
NiraliBaldaniya <niralibaldaniya17@gmail.com>
2026-04-20 12:45:27 +0530

    add quizquestions.js

---
+++ quizquestions.js
@@ -0,0 +1,103 @@
+const questions = [
+{
+question: "What is the capital of india?",
+options: [
+"Mumbai",
+"New Delhi",
+"Chennai",
+"Kolkata"
+],
+answer: "New Delhi"
+}  ,
+{
+question: "How many bones are in the adult human body?",
+options: [
+"196",
+"206",
+"212",
+"220"
+],
+answer: "206"
+},
+{
+question: "Which planet is closest to the Sun?",
+options: [
+"Mars",
+"Earth",
+"Mercury",
+"Venus"
+],
+answer: "Mercury"
+},
+{
+question: "What is a word, phrase, number, or other sequence of characters that reads the same backward as forward?",
+options: [
+"Palindrome",
+"Armstrong",
+"Factorial",
+"Arithmetic"
+],
+answer: "Palindrome"
+},
+{
+question: "What company was initially known as 'Blue Ribbon Sports'? ",
+options: [
+"Nike",
+"Adidas",
+"Puma",
+"Skechers"
+],
+answer: "Nike"
+},
+{
+question: "Which planet is known as the 'Red Planet'?",
+options: [
+"Jupiter",
+"Saturn",
+"Venus",
+"Mars"
+],
+answer: "Mars"
+}
+,
+{
+question: "What is the largest ocean on Earth?",
+options: [
+"Atlantic ocean",
+"Pacific ocean",
+"Arctic ocean",
+"Indian ocean"
+],
+answer: "Pacific ocean"
+},
+{
+question: "Who painted the 'Mona Lisa'?",
+options: [
+"Vincent van gogh",
+"Leonardo da vinci",
+"Pablo picasso",
+"Michelangelo"
+],
+answer: "Leonardo da vinci"
+},
+{
+question: "How many continents are there on Earth?",
+options: [
+"8",
+"5",
+"7",
+"6"
+],
+answer: "7"
+},
+{
+question: "What is the hardest natural substance on Earth?",
+options: [
+"Diamond",
+"Quartz",
+"Iron",
+"Gold"
+],
+answer: "Diamond"
+} 
+];
